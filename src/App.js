5738e7f968fd23472c1d7c5041247d7350d8a788
ThaoDoann <phuongthao262610@gmail.com>
2020-10-03 18:38:23 -0400

    Create a simple education page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import Home from 'shared/pages/home/home';
+import Education from 'shared/pages/education/education.jsx';
+import LessonPage from 'shared/pages/lessonPage/lessonPage.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/education" component={Education} exact /> 
+          <Route exact path="/education/:lessonID" render={(props) => <LessonPage {...props} />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
